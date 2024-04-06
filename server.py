fea80e9d5745948751af7c5f30ebe83b4d93c861
Dorfultariant <tunxero@hotmail.com>
2024-04-06 13:18:13 +0300

    Basic setup

---
+++ server.py
@@ -0,0 +1,49 @@
+import sqlite3 as sq
+from datetime import datetime as dt
+import os
+
+dbFile = "main.db"
+dbInitFile = "db_init.sql"
+
+db = None
+cur = None
+
+def initConnection():
+    global db
+    global cur
+    db = sq.connect(dbFile)
+    cur = db.cursor()
+    cur.execute("PRAGMA foreign_keys = ON;")
+    return True
+
+
+def initDB():
+    if os.path.isfile(dbFile):
+        initConnection()
+        return True
+    initConnection()
+    try:
+        f = open(dbInitFile, "r")
+        command = ""
+        for l in f.readlines():
+            command += l
+        cur.executescript(command)
+        db.commit()
+    except FileNotFoundError:
+        print(f"'{dbInitFile}' file not found. Abort!")
+        print(f"Include '{dbInitFile}' file in the same folder as databaser.py")
+        return False
+
+    except sq.Error as e:
+        print("Hmm, something went sideways. Abort!")
+        print(e)
+        return False
+    return True
+
+
+def main():
+    if not initDB(): return -1
+    print ("hello there")
+    return
+
+main()
